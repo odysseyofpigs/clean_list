7d36deef548df31a0a607b58c9c77bd7784ef611
Noe Garcia <noe.garcia@student.nmt.edu>
2019-05-26 12:01:18 -0600

    nosdupl added

--- clean_list.c
+++ clean_list.c
@@ -11,13 +11,14 @@
 #include "clean_list.h"
 
 
+
 /**
  * Function that takes an array and removes all duplicates within the array.
     The given array does not have to be sorted. The newly constructed array
     utilizes 0 as a placeholder for a NULL value within the newly constructed
     array (since static array resizing is not possible).
  * @param *a The given array
- * @param size The size (as an int) of the given array
+ * @param size The size (as an int) of the given array.
  */
 void nodupl(int *a, int size)
 {
@@ -83,6 +84,77 @@ void nodupl(int *a, int size)
                         a[i] = 0;
                 }
         }
+
+        free(b);
+
+}
+
+
+
+/** Function that takes the given array and removes all duplicates within the array.
+     The function nosdupl works similarly to the function nodupl, however, nosdupl
+     will only take an array thats elements have already been sorted. The newly
+     constructed array utilizes 0 as a placeholder for a NULL value within the
+     newly constructed array.
+ * @param *a the given array. The given array must be sorted.
+ * @param size the given size (as an int) of the array.
+ */
+void nosdupl(int *a, int size)
+{
+        /* check for error */
+        if (!a) {
+                printf("Error: array does not exist\n");
+                printf("Program terminated\n");
+                exit(0);
+        }
+
+        int i = 0;
+        int dupl = 0;
+
+        /* printf pre-traversal */
+        printf("pre-operation\n");
+        for (; i < size; i++) {
+                printf("%d ", a[i]);
+        }
+        printf("\n");
+
+        int newsize = 1;
+        int *b = malloc(sizeof(int));
+        b[0] = a[0];
+
+        for (i = 1; i < size; i++) {
+                dupl = 0;
+
+                /* check *b */
+                if (a[i] == b[newsize - 1]) {
+                        dupl = 1;
+                }
+
+                /* if no duplicate */
+                if (!dupl) {
+                        newsize++;
+                        b = realloc(b, newsize * sizeof(int));
+                        b[newsize - 1] = a[i];
+                }
+        } /* end of *a traversal */
+
+        for (i = 0; i < newsize; i++) {
+                a[i] = b[i];
+        }
+
+        if (i < size) {
+                for (; i < size; i++) {
+                        a[i] = 0;
+                }
+        }
+
+        /* print post-traversal */
+        printf("post-operation\n");
+        for (i = 0; i < size; i++) {
+                printf("%d ", a[i]);
+        }
+        printf("\n");
+
 
         free(b);
 
